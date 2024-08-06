c50c4f7af7b0584f7d853f17d8d8c01b47bca86a
sumanmajee <78966433+sumanmajee@users.noreply.github.com>
2024-08-06 10:54:51 +0530

    user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,20 @@ import mongoose from "mongoose"
 import { DB_NAME } from "./constants.js"
 import connectDB from "./db/database.js"
 import dotenv from "dotenv"
+import { app } from "./app.js"
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!!!", err);
+    
+})
